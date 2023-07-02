c4c543431ebd78808159a3441199a9c35381782e
André Silva <andreluis.9097@gmail.com>
2023-07-02 19:52:05 -0300

    Implementando as funcionalidades do menu.

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
+""" Escrever docstrings """
 from src.Endereco import Endereco
 from src.Pessoa import Pessoa
 from src.Conta import Conta
+import textwrap
 
 
-end = Endereco(
-        'Luciano das neves',
-        7, 
-        'Novo Divino',
-        'Vila Velha', 
-        'ES'
-    )
+def menu():
+    """ Escrever docstrings """
+    opcao_menu = """
+    [1] - \tDepositar
+    [2] - \tSacar
+    [3] - \tExtrato
+    [4] - \tNova conta
+    [5] - \tListar contas
+    [6] - \tNovo cliente
+    [7] - \tSair
+    """
+    print(textwrap.dedent(opcao_menu))
 
-p1 = Pessoa(
-        nome='André',
-        sobrenome='Silva',
-        data_nascimento='12-12-1989',
-        rg='3154875',
-        cpf='45874589652',
-        endereco= end
-    )
-
-c1 = Conta('4232', p1, 3500.00, 10000)
-
-c1.depositar(200)
-c1.saque(200)
-c1.saque(159.50)
-c1.extrado()
-c1.historico.imprime()
-c1.saque(100)
-c1.extrado()
-c1.historico.imprime()
+    return input()
 
 
+if __name__ == '__main__':
+    conta = Conta()
 
+    while True:
+        opcao = menu()
 
+        if opcao == '1':
+            valor = float(input('Informe o valor do depósito:\nR$ '))
+            conta.depositar(valor)
+
+        elif opcao == '2':
+            valor = float(input('Informe o valor do saque:\nR$ '))
+            conta.saque(valor)
+
+        elif opcao == '3':
+            nome = input('Nome: ')
+            sobrenome = input('Sobrenome: ')
+            data_nasc = input('Data Nasc. (dd/mm/aaaa): ')
+            rg = input('RG: ')
+            cpf = input('CPF: ')
+
+            rua = input('Rua: ')
+            numero = int(input('N° da casa: '))
+            bairro = input('Bairro: ')
+            municipio = input('Município: ')
+            estado = input('Estado: ')
+
+            endereco = Endereco(rua, numero, bairro, municipio, estado)
+            cliente = Pessoa(nome, sobrenome, data_nasc, rg, cpf, endereco)
