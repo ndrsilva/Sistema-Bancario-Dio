c439c2b3a76b3f91a88bafaeede5e8370157278d
André Silva <andreluis.9097@gmail.com>
2023-07-02 19:51:05 -0300

    Update Historico.py

--- src/Historico.py
+++ src/Historico.py
@@ -1,17 +1,20 @@
+""" Escrever docstrings """
 from datetime import datetime
 
 
 class Historico:
+    """ Escrever docstrings """
 
     def __init__(self):
         self.data_abertura = datetime.today()
         self.transacoes = []
 
     def imprime(self):
+        """ Escrever docstrings """
         print(f'Data de abertura: {self.data_abertura}')
         print('Transações: ')
 
-        for t in self.transacoes:
-            print(f'- {t}')
-            
+        for transacao in self.transacoes:
+            print(f'- {transacao}')
+
         print()
